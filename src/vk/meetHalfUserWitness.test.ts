a5df000f766bbecbd09d3d296cb870ded583ffbb
alexey2baranov <Ir1n@gh>
2021-02-13 15:13:49 +0300

    fix: fix orthography

--- src/vk/meetHalfUserWitness.test.ts
+++ src/vk/meetHalfUserWitness.test.ts
@@ -1,10 +1,10 @@
 import container from "@/di/container"
 import {VK} from "vk-io";
-import meetWitness from "@/vk/meetHalfUserWitness";
+import meetHalfUserWitness from "@/vk/meetHalfUserWitness";
 import testUser from "@entity/user/test-utils/testUser";
 import Chat from "@entity/Chat.entity";
 
-describe('meetWitness', () => {
+describe('meetHalfUserWitness', () => {
   let vk: VK,
     VK_TEST_USER = parseInt(process.env.VK_TEST_USER)
 
@@ -14,7 +14,7 @@ describe('meetWitness', () => {
   })
 
   it('success to new chat', async () => {
-    const result = await meetWitness(testUser({witness_id: VK_TEST_USER}), testUser())
+    const result = await meetHalfUserWitness(testUser({witness_id: VK_TEST_USER}), testUser())
     expect(result).toBeInstanceOf(Chat)
   })
 })
