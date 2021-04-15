3a57c5c2fe0fe897363437b976a8ce027dd34307
alexey2baranov <Ir1n@gh>
2021-04-16 00:01:14 +0300

    fix(witness): halfuser sorted , filtered by alien

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,14 @@ import {basename} from "path";
 import '@/utils/checkEnv'
 import {types} from 'pg'
 import meetHalfUserReadyToWitnessChat from "@/job/meetHalfUserReadyToWitnessChat";
-
+import {version} from '../package.json'
 
 // doc: https://github.com/typeorm/typeorm/issues/2400#issuecomment-612193003
 types.setTypeParser(20, Number);
 
 const logger = container.createLogger({name: basename(__filename)});
 
-logger.info(`process.NODE_ENV = ${process.env.NODE_ENV}`);
+logger.info(`v${version} on ${process.env.NODE_ENV}`);
 
 (async () => {
 
